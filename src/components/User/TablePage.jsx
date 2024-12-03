b2a68aab7c0e83b132e15daef749672786acd6b1
AkinKale1999 <akinkale1234@outlook.de>
2024-12-03 09:47:53 +0100

    New Features

--- src/components/User/TablePage.jsx
+++ src/components/User/TablePage.jsx
@@ -1,7 +1,9 @@
 import * as React from "react";
-import { DataGrid, renderActionsCell } from "@mui/x-data-grid";
+import { DataGrid } from "@mui/x-data-grid";
 import Paper from "@mui/material/Paper";
 import { useNavigate } from "react-router-dom";
+import ViewTableButton from "../Buttons/ViewTableButton";
+import { useDemoRouter } from "@toolpad/core/internal";
 
 const columns = [
   { field: "id", headerName: "ID", width: 70 },
@@ -20,18 +22,19 @@ const columns = [
     headerName: "Action",
     sortable: false,
     width: 160,
+    renderCell: (params) => <ViewTableButton row={params.row} />,
   },
 ];
 
 const rows = [
-   { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-   { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-   { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-   { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: 25 },
-   { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-   { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-   { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: 25 },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
 ];
 
@@ -39,14 +42,13 @@ const paginationModel = { page: 0, pageSize: 5 };
 
 export default function DataTable() {
   const navigate = useNavigate();
+  const router = useDemoRouter("/Liste");
 
-  // const router = useDemoRouter("/Liste");
-
-  // React.useEffect(() => {
-  //   if (rows.length == 0) {
-  //     navigate("/Create");
-  //   }
-  // }, []);
+  React.useEffect(() => {
+    if (rows.length === 0) {
+      navigate("/Create");
+    }
+  }, []);
 
   return (
     <Paper sx={{ height: 400, width: "100%" }}>
@@ -57,7 +59,6 @@ export default function DataTable() {
         pageSizeOptions={[5, 10]}
         checkboxSelection
         sx={{ border: 0 }}
-        // router={router}
       />
     </Paper>
   );
