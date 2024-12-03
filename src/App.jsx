feb74f63954bf9c1e7908a2829c31d18e2d827fa
AkinKale1999 <akinkale1234@outlook.de>
2024-12-03 09:43:52 +0100

    New Features

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import DashboardLayoutBranding from "./components/Dashboard/Dashboard";
-import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
+import { useState, useEffect } from "react";
+import SlotPropsSignIn from "./pages/Login";
+import Logout from "./pages/Logout";
+import "./MyApp.css";
 
 const App = () => {
-  if (localStorage.getItem("isAuthenticated") === "true") {
+  const [auth, setAuth] = useState(localStorage.getItem("Auth") === "true");
+
+  if (auth) {
     return (
-      <>
-        <Router>
-          <Routes>
-            <Route path="/" element={<DashboardLayoutBranding />} />
-            <Route path="/dashboard" element={<DashboardLayoutBranding />} />
-
-            {/* <Route path="/user-list" element={<DashboardLayoutBranding />} /> */}
-
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </Router>
-      </>
+      <Router>
+        <Routes>
+          {/* <Route path="/" element={<DashboardLayoutBranding />} /> */}
+          <Route path="/dashboard" element={<DashboardLayoutBranding />} />
+          <Route path="/" element={<Navigate to="/Dashboard" />}/>
+        </Routes>
+      </Router>
     );
   } else {
-    return <div>TRUE</div>;
+    // Wenn der Benutzer nicht authentifiziert ist, leite ihn zur Login-Seite oder Logout-Route
+    return (
+      <Router>
+        <Routes>
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/login" element={<SlotPropsSignIn />} />
+        </Routes>
+      </Router>
+    );
   }
 };
 
