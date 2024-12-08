1536ca4ae11f8303c03db0cadc6e947c2231de9e
Dein Name <deine.email@example.com>
2024-12-08 23:04:58 +0100

    New Features

--- src/app/components/arrows/ArrowUpAndDown.js
+++ src/app/components/arrows/ArrowUpAndDown.js
@@ -3,12 +3,28 @@
 import IconButton from "@mui/material/IconButton";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 export default function Arrow({ isDarkMode }) {
   const handleIconPosition = useRef(null);
   const halfCircle = useRef(null);
   const [isArrowUp, setIsArrowUp] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 600);
+    };
+
+    handleResize(); // Initial check for screen size
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const handleIconClick = () => {
     setIsArrowUp((prev) => !prev);
@@ -25,6 +41,12 @@ export default function Arrow({ isDarkMode }) {
       handleIconPosition.current.style.top = isArrowUp ? "17px" : "-30px";
     }
   };
+
+  if (isSmallScreen && handleIconPosition.current) {
+    halfCircle.current.style.rotate = isArrowUp ? "180deg" : "0deg";
+    halfCircle.current.style.top = isArrowUp ? "22px" : "25px";
+    handleIconPosition.current.style.top = isArrowUp ? "-24px" : "9px";
+  }
 
   return (
     <div id="ContainerDIV">
