cd0c2cca0583656b20c7281c7be065c94409ca0e
Dein Name <deine.email@example.com>
2024-12-22 23:00:55 +0100

    NEW FEATURES

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -43,6 +43,7 @@ const Login: React.FC = () => {
         password,
       });
       console.log("API-Endpoint:", `${process.env.NEXT_PUBLIC_BASE_URL}/login`);
+      console.log("API-Endpoint:", `${process.env.NEXT_PUBLIC_BASE_URL}`);
       console.log("Request Body:", { username, password });
 
       if (response.status === 200 && response.data.token) {
