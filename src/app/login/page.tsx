3887385a9c67057fa70293dc24542cb9d496910a
Dein Name <deine.email@example.com>
2024-12-23 02:01:56 +0100

    NEW FEATURES

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -39,7 +39,7 @@ const Login: React.FC = () => {
 
     try {
       const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_BASE_URL}login`,
+        `${process.env.NEXT_PUBLIC_BASE_URL}user/login`,
         { username, password },
         { withCredentials: true }
       );
