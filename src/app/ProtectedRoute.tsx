1007c6ef5ed0a7ebbd2e7b8d3699e62279ff8783
Dein Name <deine.email@example.com>
2024-12-25 20:48:35 +0100

    NEW FEATURES

--- src/app/ProtectedRoute.tsx
+++ src/app/ProtectedRoute.tsx
@@ -1,28 +1,42 @@
 "use client";
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import Cookies from "js-cookie"; // Importiere die js-cookie Bibliothek
+import { useRouter } from "next/navigation"; // Importiere den Router aus Next.js
 
 interface ProtectedRouteProps {
-  children: React.ReactNode;
+  children: React.ReactNode; // Erwartet, dass alle untergeordneten Elemente (children) als Prop übergeben werden
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const router = useRouter();
+  const router = useRouter(); // Der Router wird verwendet, um Weiterleitungen durchzuführen
 
   useEffect(() => {
-    const token = Cookies.get("authToken"); // Lese den Token aus den Cookies
+    // Hilfsfunktion zum Abrufen eines bestimmten Cookies
+    const getCookie = (name: string) => {
+      const value = `; ${document.cookie}`; // Greift auf alle Cookies zu
+      const parts = value.split(`; ${name}=`); // Teilt die Cookie-Zeichenkette anhand des gesuchten Cookie-Namens
+      if (parts.length === 2) return parts.pop()?.split(";").shift(); // Wenn der Cookie gefunden wird, gibt er den Wert zurück
+      return null; // Wenn der Cookie nicht existiert, gibt null zurück
+    };
+
+    // Lese das Cookie "_CTA"
+    const _CTA = getCookie("_CTA");
+
+    // Hole den aktuellen Pfad (URL-Pfad) der Seite
     const currentPath = window.location.pathname;
 
-    if (!token && currentPath !== "/login") {
-      // Wenn kein Token vorhanden ist, leite zur Login-Seite weiter
-      router.push("/login");
-    } else if (token && currentPath === "/login") {
-      // Wenn ein Token vorhanden ist und der Benutzer auf der Login-Seite ist, leite zur Customer-Seite weiter
-      router.push("/customer");
+    // Wenn das Cookie "_CTA" nicht existiert und der Benutzer nicht auf der Login-Seite ist,
+    // leite zur Login-Seite weiter
+    if (!_CTA && currentPath !== "/login") {
+      router.push("/login"); // Weiterleitung zur Login-Seite
     }
-  }, [router]);
+    // Wenn das Cookie "_CTA" existiert und der Benutzer auf der Login-Seite ist,
+    // leite zur Customer-Seite weiter
+    else if (_CTA && currentPath === "/login") {
+      router.push("/customer"); // Weiterleitung zur Customer-Seite
+    }
+  }, [router]); // Der useEffect Hook wird nur ausgelöst, wenn der Router sich ändert
 
+  // Gib die Kinder (Content) der ProtectedRoute-Komponente aus
   return <>{children}</>;
 };
 
